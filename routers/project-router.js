677edd066dbc6feb9491ac91d2c2818c00c745d5
dpayton23 <darien.payton71@gmail.com>
2019-04-26 12:13:55 -0400

    created projects endpoints

--- routers/project-router.js
+++ routers/project-router.js
@@ -12,4 +12,47 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+    newProject = req.body
+    projects.insert(newProject)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(() => {
+        res.status(400).send('You did something wrong')
+    })
+})
+
+router.delete('/:id', (req,res) => {
+    projects.remove(req.params.id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(() => {
+        res.status(400).send('You did something wrong')
+    })
+})
+
+router.put('/:id', (req, res) => {
+    projectId = req.params.id
+    changes = req.body
+    projects.update(projectId, changes)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(() => {
+        res.status(400).send('You did something wrong')
+    })
+})
+
+router.get('/:id', (req, res) => {
+    projects.getProjectActions(req.params.id)
+    .then(project => {
+        res.status(200).json(project)
+    })
+    .catch(() => {
+        res.status(400).send('You did something wrong')
+    })
+})
+
 module.exports = router
